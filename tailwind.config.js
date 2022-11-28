7bdb9d3eb2c8bd712cf536cacd7defd12f38b6a9
Brandon J Turner <me@brandonjtunrer.com>
2022-11-27 22:35:02 -0800

    vercel config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,5 @@
-// eslint-disable-next-line no-undef
 module.exports = {
   content: ['./src/**/*.{js,ts,jsx,tsx,css,scss}'],
-  // darkMode: 'media', // or 'media' or 'class'
   theme: {
     extend: {
       keyframes: {
@@ -30,6 +28,5 @@ module.exports = {
       },
     },
   },
-  // eslint-disable-next-line no-undef
   plugins: [require('@tailwindcss/forms'), require('@tailwindcss/typography')],
 };
